145da105ca507a4a2bd9794e55c3b0a1e735cb16
Brian Capouch <brianc@proto.palaver.net>
2015-05-28 02:09:28 -0400

    Alter to better follow the book

--- BetterHash.js
+++ BetterHash.js
@@ -7,13 +7,19 @@ function HashTable() {
   //this.get = get;
 }
 
-function simpleHash(data) {
+function betterHash(string) {
+  var H = 31;
   var total = 0;
-  for (var i = 0; i < data.length; ++i) {
-    total += data.charCodeAt(i);
+  for (var i = 0; i < string.length; ++i) {
+    total += H * total + string.charCodeAt(i);
   }
-  return total % this.table.length;
+  total = total % this.table.length;
+  if (total < 0) {
+    total += this.table.length-1;
+  }
+  return parseInt(total);
 }
+
 
 function put(data) {
   var pos = this.simpleHash(data);
